179c32367af1f5ef91352014f444a22cbc7a8a0e
Maxsonin <maximles.lv@gmail.com>
2024-08-16 23:40:05 +0300

    Window Creation

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,47 @@
 #include <iostream>
 
-#include <GLFW/glfw3.h>
 #include <GL/gl3w.h>
+#include <GLFW/glfw3.h>
 
 int main()
 {
-	if (!glfwInit())
-	{
-		std::cerr << "ERROR: Failed to initialize GLFW";
-		exit(EXIT_FAILURE);
-	}
+    if (!glfwInit())
+    {
+        std::cerr << "ERROR: Failed to initialize GLFW";
+        exit(EXIT_FAILURE);
+    }
 
-	const unsigned int WINDOW_WIDTH = 1000;
-	const unsigned int WINDOW_HEIGHT = 1000;
-	GLFWwindow* applicationWindow = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Grass Mania", NULL, NULL);
-	glfwMakeContextCurrent(applicationWindow);
+    const unsigned int WINDOW_WIDTH = 1000;
+    const unsigned int WINDOW_HEIGHT = 1000;
+    GLFWwindow* applicationWindow = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Grass Mania", NULL, NULL);
+    if (!applicationWindow)
+    {
+        std::cerr << "ERROR: Failed to create GLFW window";
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
 
-	if (gl3wInit())
-	{
-		std::cerr << "ERROR: Failed to Initialize gl3w";
-		exit(EXIT_FAILURE);
-	}
+    glfwMakeContextCurrent(applicationWindow);
 
-	if (glfwWindowShouldClose(applicationWindow))
-	{
-		glfwPollEvents();
+    if (gl3wInit())
+    {
+        std::cerr << "ERROR: Failed to Initialize gl3w";
+        exit(EXIT_FAILURE);
+    }
 
-		glClear(GL_COLOR_BUFFER_BIT);
+    while (!glfwWindowShouldClose(applicationWindow))
+    {
+        glfwPollEvents();
 
-		glClearColor(0.3, 0.5, 0.8, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-		glfwSwapBuffers(applicationWindow);
-	}
+        glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
 
-	return EXIT_SUCCESS;
+        glfwSwapBuffers(applicationWindow);
+    }
+
+    glfwDestroyWindow(applicationWindow);
+    glfwTerminate();
+
+    return EXIT_SUCCESS;
 }
